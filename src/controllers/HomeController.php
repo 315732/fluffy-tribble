0bde4d19983dd434713802f9646f057730c4087a
Elster <lynxr30@gmail.com>
2025-03-22 14:35:44 -0700

    Path upda ted

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -1,8 +1,9 @@
 <?php
-require_once "models/Model.php";
 
-class HomeController {
-    public function index() {
-        require "views/home.php";
+class HomeController
+{
+    public function index()
+    {         
+        require "views/Public/home.php";
     }
 }
